eeb0cff00a647b06ff0b3a52b83bbcfd05dd4517
Ben Elder <benjamin.elder@gmail.com>
2021-05-11 12:50:06 -0400

    appears that we may be missing some Gray positions

--- Kanoodle.Core/Colors/Gray.cs
+++ Kanoodle.Core/Colors/Gray.cs
@@ -16,7 +16,19 @@
                     new Node(1,0,0),  //  0 0
                     new Node(1,-1,0),
                     new Node(2,-1,0)
-                }, "Gray 1", 'K')
+                }, "Gray 1", 'K'),
+                new Piece(new Node[] {
+                    new Node(0,0,0),  //   0
+                    new Node(-1,1,0), //  0 0
+                    new Node(-1,2,0), //   X
+                    new Node(0,1,0)
+                }, "Gray 2", 'K'),
+                new Piece(new Node[] {
+                    new Node(0,0,0),   //   X
+                    new Node(0,-1,0),  //  0 0
+                    new Node(-1,-1,0), //   0
+                    new Node(1,-2,0)
+                }, "Gray 3", 'K')
             };
         }
     }
